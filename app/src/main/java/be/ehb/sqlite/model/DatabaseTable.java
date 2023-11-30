290622d8cea2cbb450eb71194035cd374c152608
VicHazewinkel <vic.hazewinkel@student.ehb.be>
2023-11-30 11:00:44 +0100

    DatabaseTable

---
+++ app/src/main/java/be/ehb/sqlite/model/DatabaseTable.java
@@ -0,0 +1,47 @@
+package be.ehb.sqlite.model;
+
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class DatabaseTable {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String naam;
+    private String kleur;
+
+    public DatabaseTable() {
+    }
+
+    @Ignore
+    public DatabaseTable(int id, String naam, String kleur) {
+        this.id = id;
+        this.naam = naam;
+        this.kleur = kleur;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getKleur() {
+        return kleur;
+    }
+
+    public void setKleur(String kleur) {
+        this.kleur = kleur;
+    }
+}
